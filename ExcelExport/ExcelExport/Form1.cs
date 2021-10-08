22a5e16060ddf8bd98cbfb16a9bae291d205d64a
Péter István Gálfi <peter.galfi@stud.uni-corvinus.hu>
2021-10-08 10:29:13 +0200

    Add Excel namespace

--- ExcelExport/ExcelExport/Form1.cs
+++ ExcelExport/ExcelExport/Form1.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace ExcelExport
 {
@@ -19,6 +15,8 @@ namespace ExcelExport
         {
             InitializeComponent();
             LoadData();
+
+            
         }
 
         public void LoadData()
