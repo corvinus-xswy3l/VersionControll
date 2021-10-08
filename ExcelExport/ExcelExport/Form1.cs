7e1c1866b92cb05cb63ac0105ad73b4de8001703
Péter István Gálfi <peter.galfi@stud.uni-corvinus.hu>
2021-10-08 10:47:36 +0200

    Create Excel

--- ExcelExport/ExcelExport/Form1.cs
+++ ExcelExport/ExcelExport/Form1.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Reflection;
+using System;
 
 namespace ExcelExport
 {
@@ -10,20 +11,51 @@ namespace ExcelExport
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> lakasok;
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
 
         public Form1()
         {
             InitializeComponent();
             LoadData();
-
-            
+            CreateExcel();
+                       
         }
 
         public void LoadData()
         {
             lakasok = context.Flats.ToList();
+        }
 
 
+        public void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+
+                CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string hiba = string.Format("Error: {0}\nLine:{1}", ex.Message, ex.Source);
+                MessageBox.Show(hiba, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+
+        private void CreateTable()
+        { 
         }
     }
 }
