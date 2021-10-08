57ab5ed5d3ab0e1f7bcb1a440a99c47b54fa86ad
Péter István Gálfi <peter.galfi@stud.uni-corvinus.hu>
2021-10-08 10:19:43 +0200

    Load Data

--- ExcelExport/ExcelExport/Form1.cs
+++ ExcelExport/ExcelExport/Form1.cs
@@ -12,9 +12,20 @@ namespace ExcelExport
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> lakasok;
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+            lakasok = context.Flats.ToList();
+
+
         }
     }
 }
