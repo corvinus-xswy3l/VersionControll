70796782296dbd9a8155eab560b82ecd4694dcc0
Péter István Gálfi <peter.galfi@stud.uni-corvinus.hu>
2021-10-22 10:34:36 +0200

    Add Portfolio

--- VaR.XSWY3L/VaR.XSWY3L/Form1.cs
+++ VaR.XSWY3L/VaR.XSWY3L/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VaR.XSWY3L.Entities;
 
 namespace VaR.XSWY3L
 {
@@ -14,11 +15,24 @@ namespace VaR.XSWY3L
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
+
         }
     }
 }
