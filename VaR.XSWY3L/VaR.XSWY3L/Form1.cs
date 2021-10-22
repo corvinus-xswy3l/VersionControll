604a5a745bb45472652b53761f733cb669312e89
Péter István Gálfi <peter.galfi@stud.uni-corvinus.hu>
2021-10-22 10:57:52 +0200

    add Portfolio Value

--- VaR.XSWY3L/VaR.XSWY3L/Form1.cs
+++ VaR.XSWY3L/VaR.XSWY3L/Form1.cs
@@ -23,6 +23,27 @@ namespace VaR.XSWY3L
             dataGridView1.DataSource = Ticks;
 
             CreatePortfolio();
+
+            int elemszám = Portfolio.Count();
+            decimal részvényekSzáma = (from x in Portfolio select x.Volume).Sum();
+            //MessageBox.Show(string.Format("Részvények száma: {0}", részvényekSzáma));
+            var otp = from x in Ticks
+                      where x.Index.Trim().Equals("OTP")
+                      select x;
+            Console.WriteLine("OTP darabszám: " + otp.Count().ToString());
+            var top = from o in otp
+                      where o.Price > 7000
+                      select o;
+            Console.WriteLine("OTP darabszám nagyobb 7000: " + otp.Count().ToString());
+            var topsum = (from t in top
+                          select t.Price).Sum();
+
+            DateTime minDátum = (from x in Ticks select x.TradingDay).Min();
+            DateTime maxDátum = (from x in Ticks select x.TradingDay).Max();
+            int elteltNapokSzáma = (maxDátum - minDátum).Days;
+            Console.WriteLine((maxDátum - minDátum).ToString());
+
+
         }
 
         private void CreatePortfolio()
@@ -33,6 +54,22 @@ namespace VaR.XSWY3L
 
             dataGridView2.DataSource = Portfolio;
 
+        }
+
+
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
         }
     }
 }
