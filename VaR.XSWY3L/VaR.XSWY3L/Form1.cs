b5b68d0f7feb1a2ed979f71def00d9531b6e6ef9
Péter István Gálfi <peter.galfi@stud.uni-corvinus.hu>
2021-10-22 10:23:22 +0200

    Get db data

--- VaR.XSWY3L/VaR.XSWY3L/Form1.cs
+++ VaR.XSWY3L/VaR.XSWY3L/Form1.cs
@@ -12,9 +12,13 @@ namespace VaR.XSWY3L
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
